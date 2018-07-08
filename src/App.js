8fa1b648e6500e0fc016ef19b229260a4a93a8db
Thanapon Jaisanuk <patpat_501@hotmail.com>
2018-07-08 14:54:01 +0700

    add mock data card picture and loop display card

--- src/App.js
+++ src/App.js
@@ -10,15 +10,67 @@ import Picturecard from './components/Picturecard'
 
 class App extends Component {
   render () {
+    const picturecard = [{
+      id: 1,
+      imgSrc: defaultImage,
+      createBy: 'Panda1',
+      date: '2018-07-07 16:11:00',
+      likeCount: 21,
+      commentCount: 10
+    },
+    {
+      id: 2,
+      imgSrc: defaultImage,
+      createBy: 'Panda2',
+      date: '2018-07-01 06:11:00',
+      likeCount: 211,
+      commentCount: 39
+    },
+    {
+      id: 3,
+      imgSrc: defaultImage,
+      createBy: 'Panda3',
+      date: '2018-06-07 10:11:00',
+      likeCount: 11,
+      commentCount: 9
+    },
+    {
+      id: 4,
+      imgSrc: defaultImage,
+      createBy: 'Panda4',
+      date: '2018-07-01 01:21:00',
+      likeCount: 1112,
+      commentCount: 401
+    },
+    {
+      id: 5,
+      imgSrc: defaultImage,
+      createBy: 'Panda5',
+      date: '2018-07-07 16:11:00',
+      likeCount: 432,
+      commentCount: 120
+    }]
+
+    const allPictureCard = picturecard.map(dataCard =>
+      <Picturecard id={dataCard.id}
+        imgSrc={dataCard.imgSrc}
+        createBy={dataCard.createBy}
+        date={dataCard.date}
+        likeCount={dataCard.likeCount}
+        commentCount={dataCard.commentCount} />
+    )
+
     return (
       <div>
         <div>
           <Navbar />
         </div>
 
-        <div>
-          <Picturecard image={defaultImage} />
-        </div>
+        <article className='container'>
+          <div className='row pt-4'>
+            {allPictureCard}
+          </div>
+        </article>
 
         <div className='App'>
           <footer className='App-header'>
