73d8428c1b4191300a65b0d50297466169091159
Thanapon Jaisanuk <patpat_501@hotmail.com>
2018-07-08 21:43:41 +0700

    add alt inside tag img

--- src/components/Picturecard.js
+++ src/components/Picturecard.js
@@ -5,7 +5,7 @@ class Picturecard extends React.Component {
     return (
       <div className='col-sm-6 col-md-3' key={this.props.id}>
         <div className='card mb-3'>
-          <img class='card-img-top' src={this.props.imgSrc} height='230px' />
+          <img class='card-img-top' src={this.props.imgSrc} height='230px' alt='' />
           <div className='card-body py-0 px-3'>
             <h5 className='card-title mt-2'>{this.props.createBy}</h5>
             <p className='card-text mb-2'>{this.props.date}</p>
