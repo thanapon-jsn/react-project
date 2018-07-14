b5b557ed8f0d52b1b541ee7d5b987847632a15b0
Thanapon Jaisanuk <thanapon.jsn@gmail.com>
2018-07-14 16:44:14 +0700

    add TODO component

---
+++ src/TODO.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/styles/app.css'
+import AddForm from './components/AddForm'
+
+class TODO extends Component {
+  render () {
+    return (
+      <AddForm />
+    )
+  }
+}
+
+export default TODO
