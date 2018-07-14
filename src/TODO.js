3a1a193fd61862c2337e7a8f7009db4c102494ca
Thanapon Jaisanuk <patpat_501@hotmail.com>
2018-07-14 23:54:39 +0700

    update TODO import to use ListTtem component

--- src/TODO.js
+++ src/TODO.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/styles/app.css'
 import AddForm from './components/AddForm'
+import ListItem from './components/ListItem'
 
 class TODO extends Component {
   render () {
     return (
-      <AddForm />
+      <div className='mx-5 my-5'>
+        <div>
+          <AddForm />
+        </div>
+        <div>
+          <ListItem />
+        </div>
+      </div>
     )
   }
 }
