64647779b76800d9a0fdb69564ce1a0a21d37857
Thanapon Jaisanuk <patpat_501@hotmail.com>
2018-07-14 16:45:46 +0700

    update use TODO component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom'
 import './assets/styles/index.css'
 import App from './App'
 import Home from './Home'
+import TODO from './TODO'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<Home />, document.getElementById('root'))
+ReactDOM.render(<TODO />, document.getElementById('root'))
 registerServiceWorker()
